c79d2c99d85034695741b5a6fe8a70bb5f9b1c84
Sudipta Mondal <msudipta857@gmail.com>
2025-05-11 12:34:24 +0530

    deployment

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/testnode',(req,res)=>{
-    res.status(200).send('Test node working be so good')
+    res.status(200).send('Test node working')
 });
 
 module.exports=app
